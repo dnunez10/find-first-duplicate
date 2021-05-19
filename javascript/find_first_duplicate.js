9f0ff4058e22a1c35fa76c3c0355503b791c8853
dnunez10 <diana_nunez10@yahoo.com>
2021-05-19 10:55:41 -0700

    Done.

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,12 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  for(let i=0; i < arr.length; i++){
+    if (arr[i] === arr[i+1]) { // comparing current and next index
+      return arr[i]
+    }
+  }
+
+  return -1
 }
 
 if (require.main === module) {
